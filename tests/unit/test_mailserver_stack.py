3597ed380c847987da6e9a60778296c937df90a7
Michele Martone <michele.martone@gmail.com>
2023-12-17 18:41:25 -0500

    implement an elastic ip on ec2

--- tests/unit/test_mailserver_stack.py
+++ tests/unit/test_mailserver_stack.py
@@ -4,11 +4,10 @@ import aws_cdk.assertions as assertions
 from mailserver.component import Mailserver
 
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in cdk/cdk_stack.py
 def test_mailserver_created():
     app = core.App()
     stack = Mailserver(app, "mailserver")
     template = assertions.Template.from_stack(stack)
 
     template.has_resource_properties("AWS::EC2::Instance", {"InstanceType": "t2.micro"})
+    template.resource_count_is("AWS::EC2::EIPAssociation", 1)
